11f5d999602846310d58fcdf3804949ea92f357e
ivucicev <ivucicev@gmail.com>
2019-09-24 15:24:25 +0200

    added encoder module, added default lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+    {
+        path: 'auth',
+        loadChildren: './modules/auth/auth.module#AuthModule'
+    },
+    {
+        path: 'encoder',
+        loadChildren: './modules/encoder/encoder.module#EncoderModule'
+    },
+    {
+        path: '**',
+        redirectTo: '/auth'
+    }
+];
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [BrowserModule, AppRoutingModule],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
