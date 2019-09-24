f3b41f60857a723ac7054d2136953ee632b9dadd
ivucicev <ivucicev@gmail.com>
2019-09-24 15:25:28 +0200

    encoder module

--- src/app/modules/encoder/encoder.module.ts
+++ src/app/modules/encoder/encoder.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EncoderComponent } from './encoder/encoder.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [EncoderComponent],
   imports: [
     CommonModule
   ]
